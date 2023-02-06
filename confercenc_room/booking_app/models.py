af9ef1fdda471178c87fedf1e7fd9dda7cd2cd83
Veturna <dpullo@outlook.com>
2023-02-06 09:45:17 +0100

    create the room and booking models

--- confercenc_room/booking_app/models.py
+++ confercenc_room/booking_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    capacity = models.PositiveIntegerField()
+    projector = models.BooleanField(default=False)
+
+
+class Booking(models.Model):
+    date = models.DateTimeField()
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    comment = models.TextField(null=True)
+
+    class Meta:
+        unique_together = ('room_id', 'date')

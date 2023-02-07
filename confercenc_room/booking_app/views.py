fb2f8b11f8d72cb892dfa5303f950ebbdc517d69
Veturna <dpullo@outlook.com>
2023-02-07 09:38:22 +0100

    add post method to add room view

--- confercenc_room/booking_app/views.py
+++ confercenc_room/booking_app/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views import View
-
+from .models import Room
 
 class AddConferenceRoom(View):
     def get(self, request):
         return render(request, "add_room.html")
     def post(self, request):
+        name = request.POST.get("name")
+        capacity = request.POST.get("capacity")
+        projector = request.POST.get("projector")
+
+        if projector == None:
+            projector = False
+        else:
+            projector = True
+
+        if str(name) is "" or int(capacity) <= 0:
+            ctx = {"result" : "Data are incorrect"}
+            return render(request, "add_room.html", ctx)
+        else:
+            try:
+                room = Room.objects.create(name=name, capacity=capacity, projector=projector)
+                return HttpResponseRedirect("/")
+            except Exception as e:
+                ctx = {"result" : "Room already exist"}
+                return render(request, "add_room.html", ctx), e
+
+
+class RoomsList(View):
+    def get(self, request):
         pass
+    def post(self, request):
+        pass
+
+
+
